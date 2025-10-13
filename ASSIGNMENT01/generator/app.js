819e8e5721d6837bc78b9c23ee99c590c0d097ed
Prince Pun <prince.pun@sungavacollege.edu.np>
2025-10-12 22:44:09 -0400

    further commit
    further commit

--- ASSIGNMENT01/generator/app.js
+++ ASSIGNMENT01/generator/app.js
@@ -1,27 +1,26 @@
 var createError = require('http-errors');
 var express = require('express');
-var app = express();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('../routers/index');
-var usersRouter = require('../routes/users');
-app.use('/', indexRouter);
+var indexRouter = require('./routers/index');
+var usersRouter = require('./routes/users');
 
-
-
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -32,11 +31,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
