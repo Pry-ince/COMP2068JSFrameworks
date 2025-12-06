27da08d9e044b056bcfadd6cb9a6f23d19290648
Prince Pun <prince.pun@sungavacollege.edu.np>
2025-12-05 20:06:19 -0500

    Added homepage and tasks

--- ASSIGNMENT02/todo-app/routes/index.js
+++ ASSIGNMENT02/todo-app/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var Task = require('../models/task');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+    try {
+        const publicTasks = await Task.find({}).limit(5).sort({createdAt: -1});
+        res.render('index', { 
+            title: 'Todo App', 
+            user: req.user, 
+            publicTasks 
+        });
+    } catch (err) {
+        res.render('index', { title: 'Todo App', user: req.user });
+    }
 });
 
 module.exports = router;
